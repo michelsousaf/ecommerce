d0bd0e6fed3e5a54da22628b8e1fa5a99ac8c7ea
Michel Fernandes <michel.sousaf@gmail.com>
2019-09-05 10:25:42 -0400

    Paginacao

--- site.php
+++ site.php
@@ -20,12 +20,32 @@ $app->get('/', function() {
 
 $app->get("/categories/:idcategory", function($idcategory){
 	//User::verifyLogin();
+
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 	$category = new Category();
+
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages =[];
+
+	//pages esta dentro de um array no metodo getProductsPage
+	for ($i=1; $i <= $pagination['pages'] ; $i++) { 
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
+
+
 	$page = new Page();
+
 	$page->setTpl("category", [
 		'category'=>$category->getValues(),
-		'products'=>Product::CheckList($category->getProducts())
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 	]);
 });
 
